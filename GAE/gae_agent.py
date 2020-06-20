9b8bc8f005ef6a747dfe51ed467a1b4c10baab75
archiexdex <rc840327@gmail.com>
2020-06-20 21:29:02 +0800

    modify ppo & add gae

--- GAE/gae_agent.py
+++ GAE/gae_agent.py
@@ -13,13 +13,14 @@ total_epoch = 10000000
 lr = 1e-2
 display_freq = 100
 gamma = 0.99
+trace_decay = 0.99
 batch_size = 32
-rw_path = "rw_ppo.npy"
-model_path = "ckpt_ppo"
+rw_path = "rw.npy"
+model_path = "best_model"
 is_resize = True
-resize_size = (80, 80)
+resize_size = (200, 200)
 device = "cuda" if torch.cuda.is_available() else "cpu"
-learning_start = 5000
+learning_start = 10000
 
 ppo_clip = 0.2
 ppo_steps = 5
@@ -94,6 +95,7 @@ class AgentPG:
             
         # discounted reward
         self.gamma = gamma
+        self.trace_decay = trace_decay
 
         # training hyperparameters
         self.total_epoch = total_epoch # total training episodes (actually too large...)
@@ -124,19 +126,15 @@ class AgentPG:
         # action = self.env.action_space.sample() # TODO: Replace this line!
         # Use your model to output distribution over actions and sample from it.
         # HINT: torch.distributions.Categorical
-        # state = torch.from_numpy(state).float().unsqueeze(0)
 
         action_pred, value_pred = self.model(state)
         action_prob = F.softmax(action_pred, dim = 1)
         m = Categorical(action_prob)
         action = m.sample()
 
-        self.states.append(state)
-        self.actions.append(action)
         self.saved_log_probs.append(m.log_prob(action))
         self.value_list.append(value_pred)
 
-        
         return action.item()
         
 
@@ -147,51 +145,42 @@ class AgentPG:
         R = 0
         eps = np.finfo(np.float32).eps.item()
         norm = lambda a: (a - a.mean()) / (a.std() + eps)
-        for r in self.rewards[::-1]:
+        for r in reversed(self.rewards):
             R = r + self.gamma * R
             r_list.append(R)
         r_list = r_list[::-1]
         r_list = torch.tensor(r_list)
         r_list = norm(r_list).to(self.device)
 
-        states = torch.cat(self.states)
-        actions = torch.cat(self.actions).to(self.device)
         saved_log_probs = torch.cat(self.saved_log_probs).to(self.device)
         v_list = torch.cat(self.value_list).squeeze(-1)
-        advantage = r_list - v_list
-        advantage = norm(advantage)
 
-        # compute PG loss
-        # loss = sum(-R_i * log(action_prob))
-        states = states.detach()
-        actions = actions.detach()
-        log_prob_actions = saved_log_probs.detach()
-        advantage = advantage.detach()
-        r_list = r_list.detach()
-        
-        for _ in range(self.ppo_steps):
-            #get new log prob of actions for all input states
-            action_pred, value_pred = self.model(states)
-            value_pred = value_pred.squeeze(-1)
-            action_prob = F.softmax(action_pred, dim = -1)
-            dist = Categorical(action_prob)
+        adv_list = []
+        advantage = 0
+        next_value = 0
 
-            #new log prob using old actions
-            new_log_prob_actions = dist.log_prob(actions)
-
-            policy_ratio = (new_log_prob_actions - log_prob_actions).exp()
-            policy_loss_1 = policy_ratio * advantage
-            policy_loss_2 = torch.clamp(policy_ratio, min = 1.0 - self.ppo_clip, max = 1.0 + self.ppo_clip) * advantage
-
-            policy_loss = - torch.min(policy_loss_1, policy_loss_2).mean()
-            value_loss = F.smooth_l1_loss(r_list, value_pred).mean()
-
-            self.optimizer.zero_grad()
+        for r, v in zip(reversed(r_list), reversed(v_list)):
+            td_error = r + next_value * self.gamma - v
+            advantage = td_error + advantage * self.gamma * self.trace_decay
+            next_value = v
+            adv_list.append(advantage)
             
-            policy_loss.backward()
-            value_loss.backward()
+        adv_list = torch.tensor(adv_list[::-1])
+        adv_list = norm(adv_list).to(self.device)
 
-            self.optimizer.step()
+        adv_list = adv_list.detach()
+        r_list = r_list.detach()        
+        
+        policy_loss = - (adv_list * saved_log_probs).sum()
+    
+        value_loss = F.smooth_l1_loss(r_list, v_list).sum()
+            
+        self.optimizer.zero_grad()
+        
+        policy_loss.backward()
+        value_loss.backward()
+        
+        self.optimizer.step()
         
     def train(self):
         st_time = datetime.now()
