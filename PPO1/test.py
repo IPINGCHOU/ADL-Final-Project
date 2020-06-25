db309bda36dfc2e9cebde74a95de0ad1b542e70e
Wang <william860707@gmail.com>
2020-06-25 20:38:35 +0800

    PPO1

---
+++ PPO1/test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+sys.path.insert(1, '../game')
+
+#os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
+MODE = 'test'
+BULLET_MODE = 'random'
+EXPOLDE_MODE = False
+PLANE_SHOW = False
+SCORE_SHOW = False
+SCREEN_SHOW = False
+#SCREEN_SHOW = True
+# test
+TEST_EPISODES = 10
+
+if SCREEN_SHOW == False:
+    os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+from game_controller import GameManager
+from ppo_agent import *
+
+env = GameManager(bullet_mode = BULLET_MODE, explode_mode=EXPOLDE_MODE, plane_show=PLANE_SHOW, score_show=SCORE_SHOW)
+agent = AgentPG(env, MODE)
+agent.load('current_model')
+agent.test(episodes=TEST_EPISODES)
