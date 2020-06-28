3b53d76f71f5bfb175842d7a00082d700eb3018f
XDEX <rc840327@gmail.com>
2020-06-28 16:30:44 +0800

    final version of Rainbow with models

--- Rainbow_GPUbuffer/utils.py
+++ Rainbow_GPUbuffer/utils.py
@@ -19,7 +19,7 @@ class NoisyLinear(nn.Module):
         self, 
         in_features: int, 
         out_features: int, 
-        std_init: float = 1,
+        std_init: float = 10,
     ):
         """Initialization."""
         super(NoisyLinear, self).__init__()
@@ -28,30 +28,36 @@ class NoisyLinear(nn.Module):
         self.out_features = out_features
         self.std_init = std_init
 
-        self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features))
-        self.weight_sigma = nn.Parameter(
-            torch.Tensor(out_features, in_features)
-        ) 
+        # self.weight_mu = nn.Parameter(torch.Tensor(out_features, in_features))
+        # self.weight_sigma = nn.Parameter(
+        #     torch.Tensor(out_features, in_features)
+        # ) 
         self.register_buffer(
             "weight_epsilon", torch.Tensor(out_features, in_features)
         )
 
-        self.bias_mu = nn.Parameter(torch.Tensor(out_features))
-        self.bias_sigma = nn.Parameter(torch.Tensor(out_features))
+        # self.bias_mu = nn.Parameter(torch.Tensor(out_features))
+        # self.bias_sigma = nn.Parameter(torch.Tensor(out_features))
         self.register_buffer("bias_epsilon", torch.Tensor(out_features))
 
-        self.reset_parameters()
+        # self.reset_parameters()
+        mu_range = 1 / math.sqrt(self.in_features)
+        self.weight_mu = nn.Parameter(torch.empty(out_features, in_features).uniform_(-mu_range, mu_range))
+        self.weight_sigma = nn.Parameter(torch.empty(out_features, in_features).fill_(self.std_init / math.sqrt(self.in_features)))
+        self.bias_mu = nn.Parameter(torch.empty(out_features).uniform_(-mu_range, mu_range))
+        self.bias_sigma = nn.Parameter(torch.empty(out_features).fill_(self.std_init / math.sqrt(self.out_features)))
+
         self.reset_noise()
 
     def reset_parameters(self):
         """Reset trainable network parameters (factorized gaussian noise)."""
         mu_range = 1 / math.sqrt(self.in_features)
-        self.weight_mu.data.uniform_(-mu_range, mu_range)
-        self.weight_sigma.data.fill_(
+        self.weight_mu.uniform_(-mu_range, mu_range)
+        self.weight_sigma.fill_(
             self.std_init / math.sqrt(self.in_features)
         )
-        self.bias_mu.data.uniform_(-mu_range, mu_range)
-        self.bias_sigma.data.fill_(
+        self.bias_mu.uniform_(-mu_range, mu_range)
+        self.bias_sigma.fill_(
             self.std_init / math.sqrt(self.out_features)
         )
 
@@ -65,20 +71,18 @@ class NoisyLinear(nn.Module):
         self.bias_epsilon.copy_(epsilon_out)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward method implementation.
-        
-        We don't use separate statements on train / eval mode.
-        It doesn't show remarkable difference of performance.
-        """
+
+        # print(self.weight_sigma[0])
         return F.linear(
             x,
             self.weight_mu + self.weight_sigma * self.weight_epsilon,
             self.bias_mu + self.bias_sigma * self.bias_epsilon,
         )
+        
     
     def scale_noise(self, size: int) -> torch.Tensor:
         """Set scale to make noise (factorized gaussian noise)."""
-        x = torch.FloatTensor(np.random.normal(loc=0.0, scale=1.0, size=size))
+        x = torch.cuda.FloatTensor(np.random.normal(loc=0.0, scale=1.0, size=size))
 
         return x.sign().mul(x.abs().sqrt())
 
